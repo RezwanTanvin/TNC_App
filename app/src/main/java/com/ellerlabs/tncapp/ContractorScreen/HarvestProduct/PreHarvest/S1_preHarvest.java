398dd5272bb81dcbfe1db6ba6064c81d1d7282c8
Rezwan <rezwanhossain10@gmail.com>
2023-03-23 17:58:34 -0400

    This version has a issue with uploading data to the server.
    New Screen S1 pre harvest

---
+++ app/src/main/java/com/ellerlabs/tncapp/ContractorScreen/HarvestProduct/PreHarvest/S1_preHarvest.java
@@ -0,0 +1,18 @@
+package com.ellerlabs.tncapp.ContractorScreen.HarvestProduct.PreHarvest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ellerlabs.tncapp.R;
+
+public class S1_preHarvest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s1_pre_harvest);
+
+
+    }
+}
